3dae1e4742efd3b078c50af5d08e9233a2de5d85
深川道陽 <fukagawa@oec.co.jp>
2025-09-19 08:27:19 +0900

    add initial project structure with .gitignore, package.json, and Prisma setup

---
+++ packages/db/prisma/migrations/20250918183413_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."Sample" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Sample_pkey" PRIMARY KEY ("id")
+);
